31775636f8e61bd4895df96f5fdc8da715e8113f
abhiraichura <raichuraabhishek3@gmail.com>
2026-06-15 01:48:20 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['gsap', 'lenis'],
   images: {
-    domains: ['images.unsplash.com', 'plus.unsplash.com'],
     unoptimized: true,
   },
   trailingSlash: true,
